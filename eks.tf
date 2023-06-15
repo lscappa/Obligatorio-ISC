565d8a7dfda4a583fd58f45b01fbdc087e7caeed
mc244130 <mc244130@fi365.ort.edu.uy>
2023-06-15 01:46:42 -0300

    eks

--- eks.tf
+++ eks.tf
@@ -10,49 +10,57 @@ resource "aws_eks_cluster" "eks-cluster-pruebi" {
         endpoint_private_access = true
         endpoint_public_access  = true
     }
+
+    tags = {
+        Name = "eks-cluster-pruebi"
+    }
+
 }
 
-resource "aws_eks_node_group" "my_node_group" {
+resource "aws_eks_node_group" "my_node_group_1" {
     cluster_name    = aws_eks_cluster.eks-cluster-pruebi.name
-    node_group_name = "my_node_group"
+    node_group_name = "my_node_group_1"
     
     node_role_arn   = "arn:aws:iam::397031304952:role/LabRole"
     
     subnet_ids      = ["subnet-0c4edfdfd5d2bec31", "subnet-09141a1130df26118"]
-    
-    # ami_type       = "AL2_x86_64"
-    instance_types   = ["t2.micro"]
-        # capacity_type  = "ON_DEMAND"
-    disk_size        = 20
 
     scaling_config {
-        desired_size = 1
+        desired_size = 2
         min_size     = 1
-        max_size     = 1
+        max_size     = 3
     }
 
-    # node_groups = [
-    #     {
-    #     name           = "node-group-1"
-    #     instance_types = ["t2.micro"]
-    #     min_size       = 1
-    #     max_size       = 3
-    #     desired_size   = 2
-    #     },
-    #     {
-    #     name           = "node-group-2"
-    #     instance_types = ["t3.micro"]
-    #     min_size       = 1
-    #     max_size       = 3
-    #     desired_size   = 2
-    #     },
-    #     {
-    #     name           = "node-group-3"
-    #     instance_types = ["t3.small"]
-    #     min_size       = 1
-    #     max_size       = 3
-    #     desired_size   = 2
-    #     }
-    # ]
+    tags = {
+        Name = "my_node_group_1"
+    }
+
+    # ami_type       = "AL2_x86_64"
+    instance_types = ["t2.micro"]
+    # capacity_type  = "ON_DEMAND"
+    # disk_size      = 20
 }
 
+# resource "aws_eks_node_group" "my_node_group_2" {
+#     cluster_name    = aws_eks_cluster.eks-cluster-pruebi.name
+#     node_group_name = "my_node_group_2"
+    
+#     node_role_arn   = "arn:aws:iam::397031304952:role/LabRole"
+    
+#     subnet_ids      = ["subnet-0c4edfdfd5d2bec31", "subnet-09141a1130df26118"]
+
+#     scaling_config {
+#         desired_size = 2
+#         min_size     = 1
+#         max_size     = 3
+#     }
+
+#     tags = {
+#         Name = "my_node_group_2"
+#     }
+
+#     # ami_type       = "AL2_x86_64"
+#     instance_types = ["t2.micro"]
+#     # capacity_type  = "ON_DEMAND"
+#     # disk_size      = 20
+# }
